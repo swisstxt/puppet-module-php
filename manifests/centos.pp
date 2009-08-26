1d3f10b3ec426a0f3c3ff2e6754688ac8f6eeedf
Simon Josi <josi@puzzle.ch>
2009-08-26 16:34:02 +0200

    syntax fix

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -12,8 +12,8 @@ class php::centos inherits php::base {
             "puppet://$server/files/php/apache/${operatingsystem}/php.conf",
             "puppet://$server/php/apache/${operatingsystem}/php.conf"
         ],
-      require => [ Package[php], Package[apache] ],
-      notify => Service[apache],
-      owner => root, group => 0, mode => 0644;
+        require => [ Package[php], Package[apache] ],
+        notify => Service[apache],
+        owner => root, group => 0, mode => 0644;
     }
 }
