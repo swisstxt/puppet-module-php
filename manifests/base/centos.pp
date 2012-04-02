708c5a4e97a4a1056f98156305c77aca4f1faf46
Simon Josi <simon.josi@atizo.com>
2012-04-02 17:20:19 +0200

    only deploy apache config if php::webserver == apache

--- manifests/base/centos.pp
+++ manifests/base/centos.pp
@@ -5,17 +5,19 @@ class php::base::centos inherits php::base {
       includepkgs => 'php*',
     }
   }
-  file{'/etc/httpd/conf.d/php.conf':
-    source => [
-      "puppet://$server/modules/site-php/apache/$operatingsystem/$fqdn/php.conf",
-      "puppet://$server/modules/site-php/apache/$operatingsystem/php.conf",
-      "puppet://$server/modules/php/apache/$operatingsystem/php.conf",
-    ],
-    require => [
-      Package['php'],
-      Package[$php::webserver],
-    ],
-    notify => Service[$php::webserver],
-    owner => root, group => 0, mode => 0644;
+  if $php::webserver == 'apache' {
+    file{'/etc/httpd/conf.d/php.conf':
+      source => [
+        "puppet://$server/modules/site-php/apache/$operatingsystem/$fqdn/php.conf",
+        "puppet://$server/modules/site-php/apache/$operatingsystem/php.conf",
+        "puppet://$server/modules/php/apache/$operatingsystem/php.conf",
+      ],
+      require => [
+        Package['php'],
+        Package[$php::webserver],
+      ],
+      notify => Service[$php::webserver],
+      owner => root, group => 0, mode => 0644;
+    }
   }
 }
