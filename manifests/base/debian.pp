d70cf3610782accd826210fc1c24161e43acd919
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2013-08-21 13:25:49 +0200

    dont manage apache related packages

--- manifests/base/debian.pp
+++ manifests/base/debian.pp
@@ -2,8 +2,4 @@ class php::base::debian inherits php::base {
   Package['php']{
     name => 'php5',
   }
-  package{'libapache2-mod-php5':
-    ensure => installed,
-    require => Package['php'],
-  }
 }
